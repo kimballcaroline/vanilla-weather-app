46f426d4c00f311c19c2d4fa5b892ea988739615
kimballcaroline <kimballcaroline@gmail.com>
2021-11-14 20:20:32 -0500

    Showing the current weather

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
 let apiKey = "9e5cdc3919987acbd4e9812ceba2a900";
+let cityName = "New York";
+let units = "imperial";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(displayTemperature);
