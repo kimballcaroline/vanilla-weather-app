fb2d4194c4853bf44f5463d109756ada9679de5d
kimballcaroline <kimballcaroline@gmail.com>
2021-11-19 19:59:44 -0500

    Added live forecast data with formatting

--- src/app.js
+++ src/app.js
@@ -22,31 +22,52 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  let day = days[date.getDay()];
+  return day;
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = ``;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
-    <div class="col">
-      <div class="WeatherForecastPreview">
-        <div class="forecast-time">${day}</div>
-        <img
-          src="https://openweathermap.org/img/wn/50d@2x.png"
-          alt="cloudy"
-          width="50"
-        />
-        <div class="forecast-temperatures">
-          <span class="forecast-temperature-max">60°</span>
-          <span class="forecast-temperature-min">40°</span>
-        </div>
-      </div>
-    </div>`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
+          <div class="col">
+            <div class="WeatherForecastPreview">
+              <div class="forecast-time">${formatDay(forecastDay.dt)}</div>
+            
+              <img
+                src="https://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png"
+                alt="cloudy"
+                width="100"
+              />
+              <div class="forecast-temperatures">
+                <span class="forecast-temperature-max">${Math.round(
+                  forecastDay.temp.max
+                )}°</span>
+                <span class="forecast-temperature-min">${Math.round(
+                  forecastDay.temp.min
+                )}°</span>
+              </div>
+            </div>
+          </div>`;
+    }
   });
-
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = `9e5cdc3919987acbd4e9812ceba2a900`;
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiURL).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -71,6 +92,7 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
   fahrenheitLink.classList.add("active");
   celsiusLink.classList.remove("active");
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -105,8 +127,6 @@ function displayFahrenheitTemperature(event) {
 }
 
 let fahrenheitTemperature = null;
-
-displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
